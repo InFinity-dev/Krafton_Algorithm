e4d1847eb43498b2f8d4988847f3dffacf664a3a
이민섭 <31795815+InFinity-dev@users.noreply.github.com>
2022-11-03 03:00:23 +0900

    4 left

--- Week 001/22_골드바흐의 추측_9020.py
+++ Week 001/22_골드바흐의 추측_9020.py
@@ -4,10 +4,38 @@
 # 10000보다 작거나 같은 모든 짝수 n에 대한 골드바흐 파티션은 존재한다.
 # 2보다 큰 짝수 n 이 주어졌을때, n의 골드바흐 파티션을 출력하는 프로그램을 작성하시오.
 # (만약 가능한 n의 골드바흐 파티션이 여러가지인 경우에는 두 소수의 차이가 가장 작은 것을 출력한다.)
+num = int(input())
 
-#테스트 케이스 개수
-t = input()
-arr = [input() for y in range(int(t))]
-print(arr)
+n = 10000
 
+# 에라토스테네스의 체
+a = [False, False] + [True] * (n - 1)
+primes = []
 
+for i in range(2, n + 1):
+    if a[i]:
+        primes.append(i)
+        for j in range(2 * i, n + 1, i):
+            a[j] = False
+
+ans_list = []
+for i in range(num):
+
+    even = int(input())
+
+    even_first = even // 2
+
+    ans1 = even_first
+    ans2 = even_first
+
+    if ans1 not in primes and ans2 not in primes:
+        while True:
+            ans1 -= 1
+            ans2 += 1
+
+            if ans1 in primes and ans2 in primes:
+                break
+
+    ans_list.append("{} {}".format(ans1, ans2))
+
+print(*ans_list, sep='\n')
--- Week 001/23_한수_1065.py
+++ Week 001/23_한수_1065.py
@@ -3,4 +3,13 @@
 # N이 주어졌을 때, 1보다 크거나 같고, N보다 작거나 같은 한수의 개수를 출력하는
 # 프로그램을 작성하시오.
 
-# 100 보다 작은 수는
+n = int(input())
+
+result = 0
+for i in range(1, n + 1):
+    arr = list(map(int, str(i)))
+    if i < 100:
+        result += 1  # 100보다 작으면 모두 한수
+    elif arr[0]-arr[1] == arr[1]-arr[2]:
+        result += 1  # x의 각 자리가 등차수열이면 한수
+print(result)
